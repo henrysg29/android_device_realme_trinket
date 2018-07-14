5973d307331d5b7e97f1aebe590879c07d015b5f
LuK1337 <priv.luk@gmail.com>
2018-07-14 11:51:57 +0000

    RMX1911: Enable OTG by default
    Change-Id: Ib25be9f50a796e5ceafc598b6f63d8e83cab6025

--- system_prop.mk
+++ system_prop.mk
@@ -25,3 +25,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
    persist.dbg.volte_avail_ovr=1 \
    persist.dbg.vt_avail_ovr=1 \
    persist.dbg.wfc_avail_ovr=1
+
+# OTG
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.oem.otg_support=true
