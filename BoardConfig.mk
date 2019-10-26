289d646e9ed7459b8f6f7c2914a8e6a06a30da8d
ganeshi4u <ganeshvarmai4u@gmail.com>
2019-10-26 08:41:01 +0200

    RMX1911: Enable verified boot

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,5 +115,9 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/qcom/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += device/qcom/sepolicy/public
 
+# Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+
 # Inherit from the proprietary version
 -include vendor/realme/RMX1911/BoardConfigVendor.mk
