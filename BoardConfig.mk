1d294b5a5f6d0b3e661216499c37608db99cdba6
ganeshi4u <ganeshvarmai4u@gmail.com>
2019-10-29 07:43:29 +0100

    RMX1911: Set and enable double tap to wake node

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,6 +107,9 @@ BOARD_USES_PRODUCTIMAGE := true
 TARGET_COPY_OUT_ODM := odm
 TARGET_COPY_OUT_VENDOR := vendor
 
+# Power
+TARGET_TAP_TO_WAKE_NODE := "proc/touchpanel/double_tap_enable"
+
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
