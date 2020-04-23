280b5f2ed1801aa9390acedd9f6c58000d1dda67
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2020-04-24 04:34:21 +0700

    RMX1911: Added RealmeParts
    * Based on https://github.com/DerpFest-Pie/device_oneplus_msm8998-common DeviceSettings
    * Added Display Color Calibration from https://github.com/anisnoorudheen/xyz/tree/pie
    Credit goes to original author
    
    Co-authored-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Co-authored-by: Stylogey <stylogey@gmail.com>

---
+++ RealmeParts/src/com/realme/realmeparts/Constants.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.realme.realmeparts;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.PreferenceManager;
+
+import android.media.AudioManager;
+
+public class Constants {
+
+    public static final Map<String, String> sStringKeyPreferenceMap = new HashMap<>();
+    public static final Map<String, Integer> sKeyDefaultMap = new HashMap<>();
+
+    public static int getPreferenceInt(Context context, String key) {
+        return Settings.System.getIntForUser(context.getContentResolver(),
+                sStringKeyPreferenceMap.get(key), sKeyDefaultMap.get(key), UserHandle.USER_CURRENT);
+    }
+
+    public static void setPreferenceInt(Context context, String key, int value) {
+        Settings.System.putIntForUser(context.getContentResolver(),
+                sStringKeyPreferenceMap.get(key), value, UserHandle.USER_CURRENT);
+    }
+}
---
+++ RealmeParts/src/com/realme/realmeparts/RealmeParts.java
@@ -0,0 +1,89 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.realme.realmeparts;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.res.Resources;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v14.preference.PreferenceFragment;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceGroup;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.TwoStatePreference;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.util.Log;
+
+import com.realme.realmeparts.kcal.KCalSettingsActivity;
+import com.realme.realmeparts.Constants;
+
+public class RealmeParts extends PreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
+
+    private static final String PREF_DEVICE_KCAL = "kcal";
+
+    public static final String KEY_VIBSTRENGTH = "vib_strength";
+
+    private Preference mKcal;
+    private VibratorStrengthPreference mVibratorStrength;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.main);
+        getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mVibratorStrength = (VibratorStrengthPreference) findPreference(KEY_VIBSTRENGTH);
+        if (mVibratorStrength != null)
+            mVibratorStrength.setEnabled(VibratorStrengthPreference.isSupported());
+
+        mKcal = findPreference(PREF_DEVICE_KCAL);
+
+        mKcal.setOnPreferenceClickListener(preference -> {
+            Intent intent = new Intent(getActivity().getApplicationContext(), KCalSettingsActivity.class);
+            startActivity(intent);
+            return true;
+        });
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Constants.setPreferenceInt(getContext(), preference.getKey(), Integer.parseInt((String) newValue));
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        // Respond to the action bar's Up/Home button
+        case android.R.id.home:
+            getActivity().finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
