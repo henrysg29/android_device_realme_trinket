21f48f25aaf4e26e5b5f9386462aee294a3d9685
Danny Lin <danny@kdrag0n.dev>
2020-09-18 22:47:02 -0700

    realme_trinket: Switch Skia rendering backend to Vulkan.
    - This results in slightly lower frame times in most UiBench tests.
    
    Change-Id: I5f7818e0e766abbde6b59adb2c182fcc6ebea250

--- system_prop.mk
+++ system_prop.mk
@@ -30,6 +30,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
+    debug.hwui.renderer=skiavk \
     debug.sf.disable_backpressure=1 \
     debug.sf.latch_unsignaled=1 \
     debug.sf.enable_hwc_vds=1
