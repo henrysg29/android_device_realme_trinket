a9e11cba6559bed925371c3a0ac08072e1ddb677
SukeeratSG <sukeeratgoindi@gmail.com>
2020-10-17 14:28:30 +0530

    Add Blur props

--- product_prop.mk
+++ product_prop.mk
@@ -30,3 +30,9 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.control_privapp_permissions=log
     
+# Blur 
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1 \
+    debug.sf.latch_unsignaled=1 \
+    debug.sf.disable_backpressure=1
